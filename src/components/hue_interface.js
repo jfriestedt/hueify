68a4024ebd72890828a329ac95437803362818c7
Jon Friestedt <jon@adaptly.com>
2019-11-21 01:34:17 -0500

    Poll detected hue bridges

--- src/components/hue_interface.js
+++ src/components/hue_interface.js
@@ -1,43 +1,69 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { isEmpty } from 'lodash';
+import { get, isEmpty } from 'lodash';
 import '../App.scss'
 
 class HueInterface extends Component {
 
-  componentDidMount () {
-    const bridgeIPs = fetch('https://discovery.meethue.com/')
-      .then((response) => response.json() )
-      .then((bridges) => {
+  async componentDidMount () {
+    const response = await fetch('https://discovery.meethue.com');
+    if (response.ok) {
+      const bridges = await response.json();
+      if (!isEmpty(bridges)) {
         this.props.dispatch({
           type: 'HUE_BRIDGES_DISCOVERED',
           payload: bridges.map((bridge) => bridge.internalipaddress)
         })
-      })
+      }
+    }
+  }
 
-    return bridgeIPs
+  // TODO: this should be a thunk action (I think?)
+  attemptUserCreations () {
+    this.props.bridgeIPs.forEach((ip) => {
+      return fetch(`http://${ip}/api`, {
+        method: 'POST',
+        body: JSON.stringify({ devicetype: 'hueify#hueify' })
+      }).then((response) => {
+        if (response.ok) {
+          return response.json().then((parsedResponse) => {
+            if (get(parsedResponse, [0, 'success'])) {
+              console.log('success!')
+            }
+          })
+        }
+      })
+    });
+  }
+
+  beginPolling () {
+    this.attemptUserCreations();
+    this.pollingTimeout = setInterval(
+      (() => this.attemptUserCreations()),
+      1000
+    );
   }
 
   componentDidUpdate (prevProps) {
-    if (isEmpty(prevProps.bridgeIps) && !isEmpty(this.props.bridgeIps)) {
-      // this.beginPolling();
+    if (isEmpty(prevProps.bridgeIPs) && !isEmpty(this.props.bridgeIPs)) {
+      this.beginPolling();
     }
   }
 
   render () {
-    return isEmpty(this.props.hueBridgeIps) ?
+    return isEmpty(this.props.bridgeIPs) ?
       null :
       <ul style={{ margin: '20px', position: 'absolute' }}>
         <h6>Philips Hue IPs:</h6>
-        {this.props.hueBridgeIps.map((ip) => {
+        {this.props.bridgeIPs.map((ip) => {
           return <li key={ip}>{ip}</li>
         })}
       </ul>
   }
 }
 
-const mapStateToProps = ({ hueBridgeIps }) => {
-  return { hueBridgeIps }
+const mapStateToProps = ({ bridgeIPs }) => {
+  return { bridgeIPs };
 }
 
 export default connect(mapStateToProps)(HueInterface);
--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux';
 import tokens from './tokens';
 import spotifyPlayerMountStatus from './spotify_player_mount_status'
 import spotifyPlayerState from './spotify_player_state'
-import hueBridgeIps from './hue_bridge_ips'
+import bridgeIPs from './hue_bridge_ips'
 import palette from './palette'
 
 export default combineReducers({
   tokens,
   spotifyPlayerMountStatus,
   spotifyPlayerState,
-  hueBridgeIps,
+  bridgeIPs,
   palette
 });
